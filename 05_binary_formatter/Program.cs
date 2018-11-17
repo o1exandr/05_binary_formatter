04480281f73c26a6d56fa2e67cdfe92e30513e06
Oleksandr Finiuk <olexandr.finyuk@gmail.com>
2018-11-17 21:43:08 +0200

    Add project files.

---
+++ 05_binary_formatter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_binary_formatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
